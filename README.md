ad581c045a615377d215618b9b4b196db08e12e3
David Keller <kellerdb@gmail.com>
2021-12-10 09:36:21 -0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# dice-roller-python
+
+Dice roller app written in Python.
+
+Use this app at [trinket.io](https://trinket.io/embed/python3/03d65faa0a?outputOnly=true&start=result).
+Learn how to make this app at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+print()
+
+num_rolls = int(input("Enter the number of rolls: "))
+
+rolls = {2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0}
+
+for i in range(num_rolls):
+  die1 = random.randint(1, 6)
+  die2 = random.randint(1, 6)
+  rolls[die1 + die2] += 1
+
+print()
+
+for roll, count in rolls.items():
+  print(f"{roll}: {count / num_rolls * 100}%")
